27dfec9a940e819c76e77521367200f4364da4a2
Wajihulhassan <wajih.lums@gmail.com>
2022-04-17 18:08:54 +0500

    fixing a minor bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 files=".emacs .emacs.d .bashrc .bash_aliases"        # list of files/folders to symlink in homedir
 
 for file in $files; do
+    if [ -L ~/$file ]; then
+	unlink ~/$file
+    fi
     echo "Creating symlink to ${file} in home directory."
     ln -s $file ~/$file
 done
